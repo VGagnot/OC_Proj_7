cd3a883a36dcd43ae16b15dfd15e0a7657a90763
VGagnot <vincent.gagnot@laposte.net>
2021-06-09 21:43:37 +0200

    Choix individu interactif, correction shap_val 1/0

--- dashboard_app/dashboard.py
+++ dashboard_app/dashboard.py
@@ -19,7 +19,6 @@ app = dash.Dash()
 
 
 echantillon_test_X = pd.read_csv("echantillon_test_X.csv")
-#echantillon_test_X = echantillon_test_X.set_index(echantillon_test_X.columns[0])
 echantillon_test_X = echantillon_test_X.drop(echantillon_test_X.columns[0], axis = 1)
 echantillon_test_y = pd.read_csv("echantillon_test_y.csv")
 
@@ -32,103 +31,14 @@ echantillon_train_X = pd.read_csv("echantillon_train_X.csv")
 echantillon_train_X = echantillon_train_X.set_index(echantillon_train_X.columns[0])
 feat_imp_glob = pd.read_csv("feat_imp_glob.csv")
 feat_imp_glob['Feature2'] = feat_imp_glob['Feature'].copy()
-feat_imp_glob = feat_imp_glob.set_index(feat_imp_glob['Feature2'])
 
 
 
 
 
-#i = 0
+i = 0
 #i = random.randrange(0, len(echantillon_test_y)-1)
-i = random.randrange(0, len(echantillon_test_y))
-
-
-data_requests = requests.get("http://127.0.0.1:5000/fi_locales/?individu="+str(i))
-json_data = data_requests.json()
-pred = json_data['pred']
-liste_contribs_loc = json_data['liste_top_10_contribs']
-val_contribs_loc = json_data['val_top_10_contribs']
-col_contribs_loc = json_data['col']
-lim_x = json_data['lim_x']
-
-
-
-#Ma jauge:
-
-seuil = 0.505
-Echelle_seuil = [0, min(0.6*seuil,1), min(0.9*seuil,1), min(seuil,1),min(1.2*seuil,1), 1]
-
-col = 'red'
-if pred < seuil:
-  col = 'green'
-else:
-  col = 'red'
-
-jauge = go.Figure(go.Indicator(
-    mode = "gauge+number",
-    value = pred,
-    domain = {'x': [0, 1], 'y': [0, 1]}
-    ,title = {'text': "Score credit"}
-    ,gauge = {'axis': {'range': [0, 1]}
-              ,'bar': {'color': col}
-              ,'steps': [
-                  {'range': [0, Echelle_seuil[1]], 'color': 'limegreen'},
-                  {'range': [Echelle_seuil[1], Echelle_seuil[2]], 'color': 'lightgreen'},
-                  {'range': [Echelle_seuil[2], Echelle_seuil[3]], 'color': 'yellow'},
-                  {'range': [Echelle_seuil[3], Echelle_seuil[4]], 'color': 'orange'},
-                  {'range': [Echelle_seuil[4], 1], 'color': 'orangered'}]
-             ,'threshold' : {'line': {'color': "black", 'width': 1}, 'thickness': 1, 'value': seuil}
-              }
-    ))
-
-
-#Principaux contributeurs:
-
-#Contributions locales
-
-contribs_loc = [go.Bar(
-   x = val_contribs_loc,
-   y = liste_contribs_loc,
-   name = 'Local feature importance',
-   marker_color = col_contribs_loc,
-   orientation='h'
-)]
-ppaux_contribs_loc = go.Figure(data=contribs_loc)
-ppaux_contribs_loc.update_layout(xaxis_range=[-lim_x[0],lim_x[0]])
-
-
-
-#Contributions locales vs globales
-
-top_10_contrib_glob_idx = np.argsort(abs(feat_imp_glob['Feature_importance']))[-10:]
-top_10_contrib_glob = feat_imp_glob.iloc[top_10_contrib_glob_idx]
-
-ppaux_contribs_glob = go.Figure()
-
-ppaux_contribs_glob.add_trace(go.Bar(
-   x = top_10_contrib_glob['Feature_importance'],
-   y = top_10_contrib_glob['Feature'],
-   name = 'Global feature importance',
-   marker_color = 'gray',
-   orientation='h'
-))
-
-ppaux_contribs_glob.add_trace(go.Bar(
-   x = json_data['contrib_top_10_glob'],
-   y = ['NEW_EXT_SOURCES_SUM_stdscl', 'EXT_SOURCE_2_stdscl', 'EXT_SOURCE_3_stdscl', 'NEW_SOURCES_PROD_stdscl', 'INSTAL_DPD_MEAN_stdscl', 'NEW_CREDIT_TO_GOODS_RATIO_stdscl', 'NEW_CREDIT_TO_ANNUITY_RATIO_stdscl', 'CODE_GENDER_stdscl', 'NEW_DOC_IND_KURT_stdscl', 'AMT_ANNUITY_stdscl'],
-   name = 'Local feature importance',
-   marker_color = json_data['col_glob'],
-   orientation='h'
-))
-
-lim_x_loc_glob = max(lim_x[1], max(top_10_contrib_glob['Feature_importance'])*1,1)
-ppaux_contribs_glob.update_layout(xaxis_range=[-lim_x_loc_glob, lim_x_loc_glob])
-
-
-
-
-
-
+#i = random.randrange(0, len(echantillon_test_y))
 
 
 
@@ -147,13 +57,13 @@ app.layout = html.Div(children=[
 	html.Div(id='display_number_output'),
 
 	dcc.Graph(id='jauge',
-		figure = jauge
+		figure = {}
               )
 	,dcc.Graph(id='ppaux_contribs_glob',
-              figure=ppaux_contribs_glob
+              figure={}
               )
 	,dcc.Graph(id='ppaux_contribs_loc',
-              figure=ppaux_contribs_loc
+              figure={}
               )
 
 	,dcc.Dropdown(id='dropdown_1', 
@@ -171,19 +81,81 @@ app.layout = html.Div(children=[
 
 
 @app.callback(
-	Output('display_number_output', 'children'),
-	Input('ID', 'value')
-	)
-def afficher_candidat(ID):
-	return u'Vous affichez le candidat "{}".'.format(ID)
-
-@app.callback(
+	Output(component_id='display_number_output', component_property='children'),
+	Output(component_id='jauge', component_property='figure'),
+	Output(component_id='ppaux_contribs_loc', component_property='figure'),
+	Output(component_id='ppaux_contribs_glob', component_property='figure'),
 	Output(component_id='scat', component_property='figure'),
+	Input(component_id='ID', component_property = 'value'),
 	Input(component_id='dropdown_1', component_property = 'value'),
 	Input(component_id='dropdown_2', component_property = 'value')
 	)
+def afficher_résultats(ID, feat_x, feat_y):
+	i = int(ID)
+	data_requests = requests.get("http://127.0.0.1:5000/fi_locales/?individu="+str(i))
+	json_data = data_requests.json()
+	#Je commence par indiquer Ma jauge:
+	pred = json_data['pred']
+	seuil = 0.505
+	Echelle_seuil = [0, min(0.6*seuil,1), min(0.9*seuil,1), min(seuil,1),min(1.2*seuil,1), 1]
+	col = 'red'
+	if pred < seuil:
+		col = 'green'
+	else:
+		col = 'red'
+	jauge = go.Figure(go.Indicator(
+		mode = "gauge+number",
+		value = pred,
+		domain = {'x': [0, 1], 'y': [0, 1]}
+		,title = {'text': "Score credit"}
+		,gauge = {'axis': {'range': [0, 1]}
+			,'bar': {'color': col}
+			,'steps': [
+				{'range': [0, Echelle_seuil[1]], 'color': 'limegreen'},
+				{'range': [Echelle_seuil[1], Echelle_seuil[2]], 'color': 'lightgreen'},
+				{'range': [Echelle_seuil[2], Echelle_seuil[3]], 'color': 'yellow'},
+				{'range': [Echelle_seuil[3], Echelle_seuil[4]], 'color': 'orange'},
+				{'range': [Echelle_seuil[4], 1], 'color': 'orangered'}]
+			,'threshold' : {'line': {'color': "black", 'width': 1}, 'thickness': 1, 'value': seuil}
+              		}
+    		))
+	#Principaux contributeurs:
+	#Limites des graphs:
+	lim_x = json_data['lim_x']
+	#Contributions locales:
+	liste_contribs_loc = json_data['liste_top_10_contribs']
+	val_contribs_loc = json_data['val_top_10_contribs']
+	col_contribs_loc = json_data['col']
+	contribs_loc = [go.Bar(
+		x = val_contribs_loc,
+		y = liste_contribs_loc,
+		name = 'Local feature importance',
+		marker_color = col_contribs_loc,
+		orientation='h'
+		)]
+	ppaux_contribs_loc = go.Figure(data=contribs_loc)
+	ppaux_contribs_loc.update_layout(xaxis_range=[-lim_x[0],lim_x[0]])
+	#Contributions locales vs globales:
+	top_10_contrib_glob_idx = np.argsort(abs(feat_imp_glob['Feature_importance']))[-10:]
+	top_10_contrib_glob = feat_imp_glob.iloc[top_10_contrib_glob_idx]
+	ppaux_contribs_glob = go.Figure()
+	ppaux_contribs_glob.add_trace(go.Bar(
+		x = top_10_contrib_glob['Feature_importance'],
+		y = top_10_contrib_glob['Feature'],
+		name = 'Global feature importance',
+		marker_color = 'gray',
+		orientation='h'
+		))
+	ppaux_contribs_glob.add_trace(go.Bar(
+		x = json_data['contrib_top_10_glob'],
+		y = ['NEW_EXT_SOURCES_SUM_stdscl', 'EXT_SOURCE_2_stdscl', 'EXT_SOURCE_3_stdscl', 		'NEW_SOURCES_PROD_stdscl', 'INSTAL_DPD_MEAN_stdscl', 'NEW_CREDIT_TO_GOODS_RATIO_stdscl', 		'NEW_CREDIT_TO_ANNUITY_RATIO_stdscl', 'CODE_GENDER_stdscl', 'NEW_DOC_IND_KURT_stdscl', 		'AMT_ANNUITY_stdscl'],
+		name = 'Local feature importance',
+		marker_color = json_data['col_glob'],
+		orientation='h'
+		))
+	lim_x_loc_glob = max(lim_x[0], max(top_10_contrib_glob['Feature_importance'])*1)
+	ppaux_contribs_glob.update_layout(xaxis_range=[-lim_x_loc_glob, lim_x_loc_glob])
 	#Scatter Plot
-def update_scatter_plot(feat_x, feat_y):
 	a = feat_x
 	b = feat_y
 	scat = go.Figure()
@@ -230,7 +202,7 @@ def update_scatter_plot(feat_x, feat_y):
 		autosize=False,
 		width=1000,
 		height=700)
-	return scat
+	return u'Vous affichez le candidat "{}".'.format(ID),jauge,ppaux_contribs_loc,ppaux_contribs_glob, scat
 
 
 
--- dashboard_app/process.py
+++ dashboard_app/process.py
@@ -11,6 +11,7 @@ lgbm2 = joblib.load(open("classification_credit.sav", 'rb'))
 testy = pd.read_csv("echantillon_test_y.csv")
 testX = pd.read_csv("echantillon_test_X.csv")
 #testX = testX.set_index(testX.columns[0])
+index = testX['Unnamed: 0'].copy()
 testX = testX.drop(testX.columns[0], axis = 1)
 
 explainerModel = shap.TreeExplainer(lgbm2)
@@ -21,8 +22,8 @@ shap_values = explainerModel.shap_values(testX)
 @app.route('/fi_locales/')
 def shap_loc_val():
 	i = int(request.args.get('individu'))
-	pred = lgbm2.predict_proba(testX.iloc[i].to_numpy().reshape(1, -1))[0][0]
-	top_10_contrib_idx = np.argsort(abs(shap_values[0][i]))[-10:]
+	pred = lgbm2.predict_proba(testX.iloc[i].to_numpy().reshape(1, -1))[0][1]
+	top_10_contrib_idx = np.argsort(abs(shap_values[1][i]))[-10:]
 	liste_contribs = []
 	val_contribs = []
 	col = []
@@ -31,23 +32,23 @@ def shap_loc_val():
 
 	for j in top_10_contrib_idx:
 		liste_contribs.append(testX.columns[j])
-		val_contribs.append(shap_values[0][i][j])
-		if shap_values[0][i][j] > 0:
+		val_contribs.append(shap_values[1][i][j])
+		if shap_values[1][i][j] > 0:
 			col.append('red')
 		else:
 			col.append('green')
 
 	contrib_top_10_glob = [
-		shap_values[0][i][testX.columns.tolist().index('NEW_EXT_SOURCES_SUM_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('EXT_SOURCE_2_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('EXT_SOURCE_3_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('NEW_SOURCES_PROD_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('INSTAL_DPD_MEAN_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('NEW_CREDIT_TO_GOODS_RATIO_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('NEW_CREDIT_TO_ANNUITY_RATIO_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('CODE_GENDER_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('NEW_DOC_IND_KURT_stdscl')],
-		shap_values[0][i][testX.columns.tolist().index('AMT_ANNUITY_stdscl')]
+		shap_values[1][i][testX.columns.tolist().index('NEW_EXT_SOURCES_SUM_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('EXT_SOURCE_2_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('EXT_SOURCE_3_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('NEW_SOURCES_PROD_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('INSTAL_DPD_MEAN_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('NEW_CREDIT_TO_GOODS_RATIO_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('NEW_CREDIT_TO_ANNUITY_RATIO_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('CODE_GENDER_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('NEW_DOC_IND_KURT_stdscl')],
+		shap_values[1][i][testX.columns.tolist().index('AMT_ANNUITY_stdscl')]
 		]
 
 	for j in contrib_top_10_glob:
@@ -62,7 +63,8 @@ def shap_loc_val():
 		max(map(abs, contrib_top_10_glob))*1.1,
 		max(map(abs, contrib_top_10_glob + val_contribs))*1.1
 		]
-		
+
+	control = testX['NEW_SOURCES_PROD_stdscl'].iloc[i]
 
 	return jsonify({
 		'pred':pred,
@@ -72,7 +74,8 @@ def shap_loc_val():
 		'col_glob': col_glob,
 		'contrib_top_10_glob': contrib_top_10_glob,
 		'lim_x': lim_x,
-		'individu': i})
+		'individu': i,
+		'control': control})
 
 
 if __name__ == "__main__":
