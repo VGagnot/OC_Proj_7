965f8e7facfa9d2b2975289cd83c5cf5c2e9e1a2
VGagnot <vincent.gagnot@laposte.net>
2021-06-17 14:17:46 +0200

    modifs mise en page dashboard + ajout reqt.txt# Please enter the commit message for your changes. Lines starting

--- dashboard_app/dashboard.py
+++ dashboard_app/dashboard.py
@@ -5,16 +5,13 @@ from dash.dependencies import Input, Output
 import requests
 import pandas as pd
 import numpy as np
-import random
-
 import joblib
 from lightgbm import LGBMClassifier
 import shap
 import plotly.graph_objs as go
-
 from flask import Flask
 
-app = dash.Dash()
+app = dash.Dash(external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 
 
@@ -37,8 +34,6 @@ feat_imp_glob['Feature2'] = feat_imp_glob['Feature'].copy()
 
 
 i = 0
-#i = random.randrange(0, len(echantillon_test_y)-1)
-#i = random.randrange(0, len(echantillon_test_y))
 
 
 
@@ -47,7 +42,7 @@ i = 0
 app.layout = html.Div(children=[
 	html.H1(children='Dashboard credit'),
 
-	html.Div(children='Bienvenue sur mon dashboard crédit.'),
+	html.Div(children=' '),
 	html.Div(["Choisissez un individu (0 à 999): ",
 		dcc.Input(id='ID', 
 			value='0', 
@@ -57,26 +52,79 @@ app.layout = html.Div(children=[
 	html.Div(id='display_number_output'),
 
 	dcc.Graph(id='jauge',
-		figure = {}
-              )
-	,dcc.Graph(id='ppaux_contribs_glob',
-              figure={}
-              )
-	,dcc.Graph(id='ppaux_contribs_loc',
-              figure={}
-              )
+		figure = {},
+		config={
+			'staticPlot': False,
+			'scrollZoom': True,
+			'doubleClick': 'reset',
+			'showTips': False,
+			'displayModeBar': False,
+			'watermark': True
+                        })
 
-	,dcc.Dropdown(id='dropdown_1', 
-		multi=False, 
-		options=[{'label': x, 'value': x} for x in sorted(echantillon_train_X.columns)],
-		value="EXT_SOURCE_1_stdscl")
-	,dcc.Dropdown(id='dropdown_2', 
-		multi=False, 
-		options=[{'label': x, 'value': x} for x in sorted(echantillon_train_X.columns)],
-		value="EXT_SOURCE_2_stdscl")
-	,dcc.Graph(id='scat',
-              figure={}
-		)
+	,html.Div([
+		dcc.Graph(id='ppaux_contribs_loc',
+			figure={}, 
+			className='six columns',
+			config={
+				'staticPlot': False,
+				'scrollZoom': True,
+				'doubleClick': 'reset',
+				'showTips': False,
+				'displayModeBar': False,
+				'watermark': True
+                        	}
+			),
+		dcc.Graph(id='ppaux_contribs_glob',
+			figure={}, 
+			className='six columns',
+			config={
+				'staticPlot': False,
+				'scrollZoom': True,
+				'doubleClick': 'reset',
+				'showTips': False,
+				'displayModeBar': False,
+				'watermark': True
+                        	}
+			),
+		])
+	,html.Div([
+		html.Div([
+			dcc.Dropdown(id='dropdown_1', 
+				multi=False,  
+				className='ten columns',
+				options=[{'label': x, 'value': x} for x in sorted(echantillon_train_X.columns)],
+				value="EXT_SOURCE_1_stdscl")
+			,dcc.Dropdown(id='dropdown_2', 
+				multi=False,  
+				className='ten columns',
+				options=[{'label': x, 'value': x} for x in sorted(echantillon_train_X.columns)],
+				value="EXT_SOURCE_2_stdscl"),
+				dcc.Graph(id='scat',
+					figure={}, 
+					className='two columns',
+					config={
+						'staticPlot': False,
+						'scrollZoom': True,
+						'doubleClick': 'reset',
+						'showTips': False,
+						'displayModeBar': False,
+						'watermark': True
+                        	})
+			],
+            		style={"width": "25%"})
+	])
+	#,html.Div([dcc.Graph(id='scat',
+				#figure={}, 
+				#className='six columns',
+				#config={
+					#'staticPlot': False,
+					#'scrollZoom': True,
+					#'doubleClick': 'reset',
+					#'showTips': False,
+					#'displayModeBar': False,
+					#'watermark': True
+                        	#})])
     ])
 
 
@@ -90,11 +138,17 @@ app.layout = html.Div(children=[
 	Input(component_id='dropdown_1', component_property = 'value'),
 	Input(component_id='dropdown_2', component_property = 'value')
 	)
-def afficher_résultats(ID, feat_x, feat_y):
+
+def afficher_résultats(ID, feat_x1, feat_y1):
+
+	# 1ère chose: extraire les données de l'individu à afficher.
+
 	i = int(ID)
 	data_requests = requests.get("http://127.0.0.1:5000/fi_locales/?individu="+str(i))
 	json_data = data_requests.json()
-	#Je commence par indiquer Ma jauge:
+
+	# Puis je construis la jauge:
+
 	pred = json_data['pred']
 	seuil = 0.505
 	Echelle_seuil = [0, min(0.6*seuil,1), min(0.9*seuil,1), min(seuil,1),min(1.2*seuil,1), 1]
@@ -119,23 +173,35 @@ def afficher_résultats(ID, feat_x, feat_y):
 			,'threshold' : {'line': {'color': "black", 'width': 1}, 'thickness': 1, 'value': seuil}
               		}
     		))
-	#Principaux contributeurs:
+
+	# 2 graphs décrivant les principaux contributeurs au score de l'individu:
+
 	#Limites des graphs:
+
 	lim_x = json_data['lim_x']
-	#Contributions locales:
+
+	#Contributions locales (principaux contributeurs pour cet individu):
+
 	liste_contribs_loc = json_data['liste_top_10_contribs']
 	val_contribs_loc = json_data['val_top_10_contribs']
 	col_contribs_loc = json_data['col']
-	contribs_loc = [go.Bar(
+	contribs_loc = go.Figure(go.Bar(
 		x = val_contribs_loc,
 		y = liste_contribs_loc,
 		name = 'Local feature importance',
 		marker_color = col_contribs_loc,
 		orientation='h'
-		)]
+		))
+	contribs_loc.update_layout(
+		title="Principaux contributeurs pour cet individu",
+		font=dict(
+			size=9
+			))
 	ppaux_contribs_loc = go.Figure(data=contribs_loc)
 	ppaux_contribs_loc.update_layout(xaxis_range=[-lim_x[0],lim_x[0]])
-	#Contributions locales vs globales:
+
+	#Contributions locales vs globales (contributions des features habituellement les plus contributifs):
+
 	top_10_contrib_glob_idx = np.argsort(abs(feat_imp_glob['Feature_importance']))[-10:]
 	top_10_contrib_glob = feat_imp_glob.iloc[top_10_contrib_glob_idx]
 	ppaux_contribs_glob = go.Figure()
@@ -148,16 +214,31 @@ def afficher_résultats(ID, feat_x, feat_y):
 		))
 	ppaux_contribs_glob.add_trace(go.Bar(
 		x = json_data['contrib_top_10_glob'],
-		y = ['NEW_EXT_SOURCES_SUM_stdscl', 'EXT_SOURCE_2_stdscl', 'EXT_SOURCE_3_stdscl', 		'NEW_SOURCES_PROD_stdscl', 'INSTAL_DPD_MEAN_stdscl', 'NEW_CREDIT_TO_GOODS_RATIO_stdscl', 		'NEW_CREDIT_TO_ANNUITY_RATIO_stdscl', 'CODE_GENDER_stdscl', 'NEW_DOC_IND_KURT_stdscl', 		'AMT_ANNUITY_stdscl'],
+		y = ['NEW_EXT_SOURCES_SUM_stdscl',
+			'EXT_SOURCE_2_stdscl',
+			'EXT_SOURCE_3_stdscl',
+			'NEW_SOURCES_PROD_stdscl',
+			'INSTAL_DPD_MEAN_stdscl',
+			'NEW_CREDIT_TO_GOODS_RATIO_stdscl',
+			'NEW_CREDIT_TO_ANNUITY_RATIO_stdscl',
+			'CODE_GENDER_stdscl',
+			'NEW_DOC_IND_KURT_stdscl',
+			'AMT_ANNUITY_stdscl'],
 		name = 'Local feature importance',
 		marker_color = json_data['col_glob'],
 		orientation='h'
 		))
 	lim_x_loc_glob = max(lim_x[0], max(top_10_contrib_glob['Feature_importance'])*1)
-	ppaux_contribs_glob.update_layout(xaxis_range=[-lim_x_loc_glob, lim_x_loc_glob])
-	#Scatter Plot
-	a = feat_x
-	b = feat_y
+	ppaux_contribs_glob.update_layout(
+		xaxis_range=[-lim_x_loc_glob, lim_x_loc_glob],
+		title="Performance comparée aux principaux contributeurs habituels",
+		font=dict(
+			size=9
+		))
+
+	# Scatter Plot pour comparaison de l'individu sélectionné avec un échantillon représentatif de la pop totale, pour 2 features sélectionnés
+	a = feat_x1
+	b = feat_y1
 	scat = go.Figure()
 	scat.add_trace(go.Scatter(x=echantillon_train_X[echantillon_train_pred['Classe'] == 0][a],
                                 y=echantillon_train_X[echantillon_train_pred['Classe'] == 0][b],
@@ -202,6 +283,8 @@ def afficher_résultats(ID, feat_x, feat_y):
 		autosize=False,
 		width=1000,
 		height=700)
+	scat.update_xaxes(title_text=feat_x1)
+	scat.update_yaxes(title_text=feat_y1)
 	return u'Vous affichez le candidat "{}".'.format(ID),jauge,ppaux_contribs_loc,ppaux_contribs_glob, scat
 
 
